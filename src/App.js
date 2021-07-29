edb899802aa530b0f4fd4a5a8dcaa7ed75806182
rajat-saxena-16 <rajat.saxena.16@mountblue.tech>
2021-07-29 14:29:30 +0530

    Added the provider from react redux to send the props to all the components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
 import './App.css';
+import { Provider } from 'react-redux';
+
+import store from './redux/store';
+
 import React from 'react';
 import Loggedin from './components/Loggedin';
 import Bar from './components/Navbar';
@@ -6,6 +10,7 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import Home from "./components/Homepage";
 
+import Cake from "./components/Cake";
 
 import {
   BrowserRouter as Router,
@@ -17,78 +22,82 @@ import Individual from './components/Individual';
 class App extends React.Component {
   render() {
     return (
-      <Router>
+      <Provider store={store}>
+        <Router>
 
-        <div className="App" >
+          <div className="App" >
 
-          < Route path="/" exact render={(props) => {
-            return (
-              <>
-                <Bar />
-                <Home />
-              </>
-            )
-          }} />
+            < Route path="/" exact render={(props) => {
+              return (
+                <>
+                  <Bar />
+                  <Home />
+                </>
+              )
+            }} />
 
-          < Route path="/login" render={(props) => {
-            return (
-              <div>
-                <Bar />
-                <Login />
-              </div>
-            )
-          }} />
+            < Route path="/login" render={(props) => {
+              return (
+                <div>
+                  <Bar />
+                  <Login />
+                </div>
+              )
+            }} />
 
-          < Route path="/signup" render={(props) => {
-            return (
-              <Signup />
-            )
-          }} />
+            < Route path="/signup" render={(props) => {
+              return (
+                <Signup />
+              )
+            }} />
 
-          < Route path="/loggedin" exact render={() => {
-            return (
-              <Loggedin />
-            )
-          }} />
+            < Route path="/loggedin" exact render={() => {
+              return (
+                <Loggedin />
+              )
+            }} />
 
-          < Route path="/loggedin/dashboard" exact render={() => {
-            return (
-              <Loggedin />
-            )
-          }} />
-
-
-          < Route path="/loggedin/dashboard/:id" render={() => {
-            return (
-              // <Loggedin />
-              <Individual />
-            )
-          }} />
-
-          < Route path="/loggedin/addexpenses" exact render={(props) => {
-            return (
-              <Loggedin {...props} />
-            )
-          }} />
+            < Route path="/loggedin/dashboard" exact render={(props) => {
+              return (
+                <Loggedin data={props}/>
+              )
+            }} />
 
 
+            < Route path="/loggedin/dashboard/:id" render={() => {
+              return (
+                // <Loggedin />
+                <Individual />
+              )
+            }} />
+
+            < Route path="/loggedin/addexpenses" exact render={(props) => {
+              return (
+                <Loggedin {...props} />
+              )
+            }} />
 
 
-          < Route path="/loggedin/recent" exact render={(props) => {
-            return (
-              <Loggedin />
-            )
-          }} />
-
-          < Route path="/loggedin/allexpenses" exact render={() => {
-            return (
-              <Loggedin />
-            )
-          }} />
 
 
-        </div >
-      </Router>
+            < Route path="/loggedin/recent" exact render={(props) => {
+              return (
+                <Loggedin />
+              )
+            }} />
+
+            < Route path="/loggedin/allexpenses" exact render={() => {
+              return (
+                <Loggedin />
+              )
+            }} />
+            <Route path="/cake" exact component={Cake}></Route>
+
+
+          </div >
+        </Router>
+      </Provider>
+
     );
   }
 }
