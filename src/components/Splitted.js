e8c803d4675f54d66eb46435b667704117b2d3d4
rajat-saxena-16 <rajat.saxena.16@mountblue.tech>
2021-07-30 18:12:10 +0530

    Added rounded image on dashhtab and corrected css fro allexpenses

--- src/components/Splitted.js
+++ src/components/Splitted.js
@@ -14,11 +14,13 @@ export default class Splitted extends React.Component {
 
             <div className="pt-3 pb-3 border-2 border-bottom">
                 <div className="row">
-                    < Link to={`/loggedin/friends/${friendName}`} class="row">
-                        <div class="col-2 align-self-center">
-                            <img src="https://s3.amazonaws.com/splitwise/uploads/category/icon/square_v2/uncategorized/general@2x.png" height="35px" alt="generalIcon" />
+                    < Link to={`/loggedin/friends/${friendName}`} id="link-decoration" class="row">
+                        <div class="col-2 align-self-center ">
+                            <img class="rounded-circle" src="https://s3.amazonaws.com/splitwise/uploads/user/default_avatars/avatar-grey2-100px.png" height="35px" alt="generalIcon" />
                         </div>
-                        <div className="col text-start"><strong>{friendName}</strong><br /><font color={`${selfPaid ? "#5bc5a7" : "#ff652f"} `}>{selfPaid ? `Owes you Rs ${equalSplit}` : `You owe Rs ${equalSplit}`}</font> </div>
+                        <div className="col text-start">
+                            <strong>{friendName}</strong><br /><font color={`${selfPaid ? "#5bc5a7" : "#ff652f"} `}>{selfPaid ? `Owes you Rs ${equalSplit}` : `You owe Rs ${equalSplit}`}</font>
+                        </div>
                     </Link >
                 </div>
             </div >
